09b6ed78a00d028fc35048d63f5781d4fdef38ec
MariaKM <merzhanova@gmail.com>
2024-08-22 22:24:06 +0300

    init: check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if (str.length % 2 !== 0 || str.length < 1) {
+    return false;
+  }
+
+  let configArr = [];
+  let pattern = '';
+
+  if (/\d/.test(str)) {
+    for (arr of bracketsConfig) {
+      configArr.push(arr.join().replace(',', ''));
+    }
+
+    for (e of configArr) {
+      pattern += e + '|';
+    }
+    pattern = pattern.replace(/\|$/, "");
+  } else {
+    for (arr of bracketsConfig) {
+      configArr.push(arr.join().replace(',', '\\'));
+    }
+
+    for (e of configArr) {
+      pattern += '\\' + e + '|';
+    }
+    pattern = pattern.replace(/\|$/, "");
+  }
+
+  const regex = new RegExp(pattern, 'g');
+
+  while (regex.test(str)) {
+    str = str.replace(regex, "");
+  }
+  return str.length === 0;
 }
